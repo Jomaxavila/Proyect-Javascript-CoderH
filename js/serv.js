08564b484eeae9626ec8ab3f3144b803475329a6
Maxi Avila <jomaxavila@gmail.com>
2023-02-06 18:48:08 -0300

    mejorando rutas de img

--- js/serv.js
+++ js/serv.js
@@ -3,7 +3,7 @@ const servicios = [
 		nombre: "BODAS",
 		precio: 200000,
 		descripcion: "Cobertura de boda completa, entrega de material en 4k, minimo de 500 fotos a los 30 dias del evento",
-		img: "img/boda.jpg",
+		img: "img/bodas.jpg",
 		cantidad: 1,
 		video: "https://www.youtube.com/embed/QkNpuTs9Q6U",
 
@@ -34,7 +34,7 @@ const servicios = [
 		nombre: "RETRATOS",
 		precio: 25000,
 		descripcion: "Sesion fotografica para porfolio profesional, redes sociales, entrega de material en 4k, minimo de 100 fotos, a los 7 dias del evento",
-		img: "img/retrato.jpg",
+		img: "img/retratos.jpg",
 		cantidad: 1,
 		video: "https://www.youtube.com/embed/WnLBxx4t0o8",
 
@@ -45,7 +45,7 @@ const servicios = [
 		nombre: "PRODUCTOS",
 		precio: 25000,
 		descripcion: "Fotografia de productos de tu empresa, entrega de material en 4k, minimo de 100 fotos, a los 7 dias del evento",
-		img: "img/producto.jpg",
+		img: "img/productos.jpg",
 		cantidad: 1,
 		video: "https://www.youtube.com/embed/YbQdCkltct8"
 
@@ -55,7 +55,7 @@ const servicios = [
 		nombre: "EMPRESAS",
 		precio: 25000,
 		descripcion: "Fotografia de empresas, entrega de material en 4k, minimo de 100 fotos, a los 7 dias del evento",
-		img: "img/empresa.jpg",
+		img: "img/empresas.jpg",
 		cantidad: 1,
 		video: `https://www.youtube.com/embed/_SYPdaR93eE&t=23s`,
 
